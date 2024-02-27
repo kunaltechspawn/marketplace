b9ff9b0ddddfb1e4d01a3af0f3d6dc8d675d96d7
Techspawn <rahulb@techspawn.co>
2024-02-27 18:01:29 +0530

    build changes

--- app/shopify.server.js
+++ app/shopify.server.js
@@ -3,6 +3,7 @@ import {
   AppDistribution,
   DeliveryMethod,
   shopifyApp,
+  LATEST_API_VERSION,
 } from "@shopify/shopify-app-remix/server";
 import { PrismaSessionStorage } from "@shopify/shopify-app-session-storage-prisma";
 import { restResources } from "@shopify/shopify-api/rest/admin/2023-10";
@@ -11,75 +12,28 @@ import prisma from "./db.server";
 const shopify = shopifyApp({
   apiKey: process.env.SHOPIFY_API_KEY,
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
-  // @ts-ignore
-  apiVersion: "2023-10",
+  apiVersion: LATEST_API_VERSION,
   scopes: process.env.SCOPES?.split(","),
   appUrl: process.env.SHOPIFY_APP_URL || "",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
-  distribution: AppDistribution.SingleMerchant,
+  distribution: AppDistribution.AppStore,
   restResources,
   webhooks: {
     APP_UNINSTALLED: {
       deliveryMethod: DeliveryMethod.Http,
       callbackUrl: "/webhooks",
     },
-    /*PRODUCTS_UPDATE: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/webhooks",
-    },*/
-    ORDERS_UPDATED: {
-      deliveryMethod: DeliveryMethod.Http,
-      callbackUrl: "/webhooks",
-    },
   },
-  /*logger: {
-    level: LogSeverity.Debug,
-  },*/
   hooks: {
-    afterAuth: async ({ session, admin }) => {
-      // shopify.registerWebhooks({ session });
-
-      // // Create webhook subscription for metaobjects since it doesnt exist on api
-      // const { accessToken, shop } = session;
-      // try {
-      //   const response = await fetch(
-      //     `https://${shop}/admin/api/${apiVersion}/graphql.json`,
-      //     {
-      //       method: "POST",
-      //       headers: {
-      //         "Content-Type": "application/json",
-      //         "X-Shopify-Access-Token": accessToken,
-      //       },
-      //       body: JSON.stringify({
-      //         query: `mutation {
-      //           webhookSubscriptionCreate1: webhookSubscriptionCreate(
-      //             topic: METAOBJECTS_UPDATE
-      //             subTopic: "type:vendors"
-      //             webhookSubscription: {callbackUrl: "${process.env.SHOPIFY_APP_URL}/webhooks", format: JSON}
-      //           ) {
-      //             userErrors {
-      //               field
-      //               message
-      //             }
-      //             webhookSubscription {
-      //               id
-      //             }
-      //           }
-      //         }`,
-      //       }),
-      //     },
-      //   );
-      //   const responseJson = await response.json();
-      //   console.log(JSON.stringify(responseJson));
-      // } catch (error) {
-      //   console.log(JSON.stringify(error));
-      // }
+    afterAuth: async ({ session }) => {
+      shopify.registerWebhooks({ session });
     },
   },
   future: {
     v3_webhookAdminContext: true,
     v3_authenticatePublic: true,
+    unstable_newEmbeddedAuthStrategy: true,
   },
   ...(process.env.SHOP_CUSTOM_DOMAIN
     ? { customShopDomains: [process.env.SHOP_CUSTOM_DOMAIN] }
@@ -87,7 +41,7 @@ const shopify = shopifyApp({
 });
 
 export default shopify;
-export const apiVersion = "2023-10";
+export const apiVersion = LATEST_API_VERSION;
 export const addDocumentResponseHeaders = shopify.addDocumentResponseHeaders;
 export const authenticate = shopify.authenticate;
 export const unauthenticated = shopify.unauthenticated;
